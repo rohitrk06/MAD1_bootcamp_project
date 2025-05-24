f90bad900f3eb778fae572289fc916dc42dcfded
Rohit Kumar <rohitkumar.rk0600@gmail.com>
2025-05-24 20:13:07 +0530

    last session

--- controllers/admin_routes.py
+++ controllers/admin_routes.py
@@ -8,7 +8,8 @@ from datetime import datetime
 def home():
     if 'email' in session:
         if session['email'] == 'admin@gmail.com':
-            return render_template('admin_home.html')
+            categories = Category.query.all()
+            return render_template('admin_home.html', categories=categories)
         else: 
             return render_template('user_home.html')
     else:
@@ -49,4 +50,50 @@ def add_category():
         # raise confirmation that category is added
         return redirect('/')
 
+
+@app.route('/edit_category/<int:category_id>', methods = ['GET', 'POST'])
+def edit_category(category_id):
+    category = Category.query.get(category_id)
     
+    if not category:
+        # category not found
+        return redirect('/')
+    
+    if request.method == 'GET':
+        return render_template('edit_category.html', category=category)
+    
+    if request.method == 'POST':
+        category_name = request.form['category_name']
+        category_description = request.form['description']
+
+        if not category_name:
+            # raise category name required
+            print("category name required")
+            return render_template('edit_category.html', category=category)
+        
+        existing_category = Category.query.filter_by(category_name=category_name).first()
+        if existing_category and existing_category.category_id != category_id:
+            # raise category already exists
+            print("category already exists")
+            return render_template('edit_category.html', category=category)
+        
+        category.category_name = category_name
+        category.category_description = category_description
+        db.session.commit()
+        # raise confirmation that category is updated
+        return redirect('/')
+
+
+@app.route('/delete_category/<int:category_id>', methods=['GET'])
+def delete_category(category_id):
+    category = Category.query.get(category_id)
+    
+    if not category:
+        # category not found
+        return redirect('/')
+    
+    db.session.delete(category)
+    db.session.commit()
+    
+    # raise confirmation that category is deleted
+    return redirect('/')
